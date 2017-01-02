05a147b83a50c85c1847c21a8332005d788b2c0f
florian bellazouz <florian.bellazouz@gmail.com>
2017-01-02 10:54:34 +0100

    add index - function documentation

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const {needTween} = require('./lib/transition')
 const {assign} = require('./lib/funcs')
 
+// mute :: (el:Object, ...styles:ArrayObject)=>el:Object
+// morph the elements to the target style with transitions or animations
 const mute = (el,...styles)=>{
   const style = assign.apply(0,styles)
   if(!needTween(style)) return assign(el,style)
 }
+
+// TODO
+// style :: (el:Object, ...styles:ArrayObject)=>el:Object
+// return a object with key __style__ that contain all datas to mute/animate it
+const style = (el,...styles)=>el
+
+// TODO
+// mergeStyle :: el:Object=>newEl:Object=>el:Object
+// return a object with style override by the newEl
+const mergeStyle = el=>newEl=>el
 
 module.exports = {mute}
