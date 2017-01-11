c30870e60ee17fd88c804b5648038f5750c659c7
florian bellazouz <florian.bellazouz@gmail.com>
2017-01-11 16:19:02 +0100

    add test/switch

---
+++ test/switch.js
@@ -0,0 +1,44 @@
+const Switch = require('../lib/switch')
+const Should = require('chai').Should()
+const Tween = require('tween.js')
+
+describe.only('.switchKernelByProp',()=>{
+  it('should apply old style',()=>{
+    let el = {x:0,y:0}
+
+    const oldValue = 10
+    const runningTween = undefined
+    const newStyle = undefined
+
+    const newEl = Switch.kernelByProp(oldValue,newStyle,runningTween)
+    newEl.x.should.be.equal(10)
+  })
+
+  it('should apply running style and transfertTween',()=>{
+    let el = {x:0,y:0}
+
+    const oldValue = 10
+    const runningTween = new Tween.Tween({x:0}).to({x:100},100).start(0)
+    const newStyle = undefined
+
+    runningTween.update(20)
+    const newEl = Switch.kernelByProp(oldValue,newStyle,runningTween)
+    newEl.x.should.be.equal(20)
+    runningTween.update(50)
+    newEl.x.should.be.equal(50)
+  })
+
+  it('should apply running style and transfertTween',()=>{
+    let el = {x:0,y:0}
+
+    const oldValue = undefined
+    const runningTween = new Tween.Tween({x:0}).to({x:100},100).start(0)
+    const newStyle = undefined
+
+    runningTween.update(20)
+    const newEl = Switch.kernelByProp(oldValue,newStyle,runningTween)
+    newEl.x.should.be.equal(20)
+    runningTween.update(50)
+    newEl.x.should.be.equal(50)
+  })
+})
